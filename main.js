4f3176ac789fb028dec89884d7a0b50ac81da58a
kahturibio <104437741+kahturibio@users.noreply.github.com>
2024-04-03 18:59:07 -0300

    klklkl

--- main.js
+++ main.js
@@ -22,5 +22,7 @@ let minutos = Math.floor(segundos / 60);
 let horas = Math.floor(minutos / 60);
 let dias = Math.floor(horas / 24);
 console.log(segundos);
+console.log(horas);
+console.log(dias);
 
 
