94fcc860e966d98bd885689d6293a3e1a467242d
kahturibio <104437741+kahturibio@users.noreply.github.com>
2024-04-03 19:09:26 -0300

    hjj

--- main.js
+++ main.js
@@ -21,8 +21,10 @@ let segundos = Math.floor(tempoFinal / 1000);
 let minutos = Math.floor(segundos / 60);
 let horas = Math.floor(minutos / 60);
 let dias = Math.floor(horas / 24);
-console.log(segundos);
-console.log(horas);
-console.log(dias);
+
+segundos %= 60;
+minutos %= 60;
+horas %= 24;
+
 
 
