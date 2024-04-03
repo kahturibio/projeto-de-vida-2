94c64d7af7c175281e49306407b80356b19d7c99
kahturibio <104437741+kahturibio@users.noreply.github.com>
2024-04-03 19:36:58 -0300

    jkk

--- main.js
+++ main.js
@@ -16,18 +16,18 @@ const contadores = document.querySelectorAll(".contador");
 let tempoAtual = new Date(); //retorna data atual do computador
 const tempoObjetivo1 = new Date("2024-05-03T00:00:00");
 
-function calculaTempo(tempoObjetivo){
-let tempoFinal = tempoObjetivo - tempoAtual;
+function calculaTempo(tempoObjetivo) {
+    let tempoFinal = tempoObjetivo - tempoAtual;
 
-let segundos = Math.floor(tempoFinal / 1000);
-let minutos = Math.floor(segundos / 60);
-let horas = Math.floor(minutos / 60);
-let dias = Math.floor(horas / 24);
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
 
-segundos %= 60;
-minutos %= 60;
-horas %= 24;
-return dias + " Dias " + horas + " Horas " + minutos + " minutos " + segundos + " segundos "
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+    return dias + " Dias " + horas + " Horas " + minutos + " minutos " + segundos + " segundos "
 
 }
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
