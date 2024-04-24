b3d0bb3188fa0bc71809741a9d1fea881b0f36f6
kahturibio <104437741+kahturibio@users.noreply.github.com>
2024-04-24 18:37:33 -0300

    encontro dia 24

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
+
 //iterando (percorrer elementos de uma lista) sobre a lista de botoes 
 for (let i = 0; i < botoes.length; i++) {
     botoes[i].onclick = function () {
@@ -13,23 +14,46 @@ for (let i = 0; i < botoes.length; i++) {
 }
 //document.querySelector("#minhaDiv p").textContent = "Olá Chuva!";
 const contadores = document.querySelectorAll(".contador");
-let tempoAtual = new Date(); //retorna data atual do computador
-const tempoObjetivo1 = new Date("2024-05-03T00:00:00");
+const tempoObjetivo1 = new Date("2024-04-19T00:00:00"); // formato de data padrao com hifem e um T para tempo
+const tempoObjetivo2 = new Date("2024-07-07T00:00:00");
+const tempoObjetivo3 = new Date("2024-04-27T00:00:00");
+const tempoObjetivo4 = new Date("2024-04-23T00:00:00");
+
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
 function calculaTempo(tempoObjetivo) {
-    let tempoFinal = tempoObjetivo - tempoAtual;
+    const tempoAtual = new Date(); //retorna data atual do computador
 
-    let segundos = Math.floor(tempoFinal / 1000);
+    let tempofinal = tempoObjetivo - tempoAtual;
+
+    let segundos = Math.floor(tempofinal / 1000);
     let minutos = Math.floor(segundos / 60);
     let horas = Math.floor(minutos / 60);
     let dias = Math.floor(horas / 24);
 
-    segundos %= 60;
+    segundos %= 60; // sinal de porcentagem traz o resto da divisao
     minutos %= 60;
     horas %= 24;
-    return dias + " Dias " + horas + " Horas " + minutos + " minutos " + segundos + " segundos "
+    if (tempofinal > 0){
+        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";}
+        else{
+            return "Prazo Finalizado!"
+        }
+    }
 
+function atualizaCronometro() {
+    for (let i = 0; i < contadores.length; i++) {
+        contadores[i].textContent = calculaTempo(tempos[i]);
+    }
 }
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
+function comecaCronometro() {
+    atualizaCronometro();
+    setInterval(atualizaCronometro, 1000); //  1000 por que em um segundo tem 1000 milissegundos
+}
+
+comecaCronometro();
+
+// o return tem a função de interromper uma funcao... se a coisa for verdadeira, ele nem busca o resto...
 
 
